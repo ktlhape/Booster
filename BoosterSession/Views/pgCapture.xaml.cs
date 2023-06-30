9bac13664c6fc58c6f7196bcafbccc649d2fdc0d
ktlhape <ktlhape@iie.ac.za>
2023-06-30 09:11:36 +0200

    Bosster session application

---
+++ BoosterSession/Views/pgCapture.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BoosterSession.Views
+{
+    /// <summary>
+    /// Interaction logic for pgCapture.xaml
+    /// </summary>
+    public partial class pgCapture : Page
+    {
+        public pgCapture()
+        {
+            InitializeComponent();
+        }
+
+      
+    }
+}
